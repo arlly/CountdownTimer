139267ed04607bc698159b7b768ab147a321b897
FuminoriArimoto <arimoto@queserser.co.jp>
2020-01-01 13:03:17 +0900

    サウンドを追加した。

--- app/src/main/java/com/example/countdowntimer/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.countdowntimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -16,12 +19,26 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCountDown;  //アクセス修飾子
     private Button mButtonStartPause;
     private Button mButtonReset;
+    private final static String TAG_START = "0";
+    private final static String TAG_PAUSE = "1";
+    private final static String TAG_RESET = "2";
 
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
 
     private long mTimerLeftInMills = START_TIME_IN_MILLIS;
 
+    /**
+     * Sound
+     **/
+    private final static String TAG_SOUND1 = "decision12";
+    private final static String TAG_SOUND2 = "decision16";
+    private final static String TAG_SOUND3 = "decision21";
+    private final static String TAG_SOUND4 = "decision24";
+
+    private MediaPlayer mediaPlayer;
+    private SoundPool soundPool;
+    private int soundId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +49,16 @@ public class MainActivity extends AppCompatActivity {
         mButtonStartPause = findViewById(R.id.button_start_pause);
         mButtonReset = findViewById(R.id.buttonreset);
 
+        soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
+
         mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mTimerRunning) {
                     pauseTimer();
+                    playSound(TAG_SOUND2);
                 } else {
+                    playSound(TAG_SOUND1);
                     startTimer();
                 }
             }
@@ -47,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 resetTimer();
+                playSound(TAG_SOUND3);
             }
         });
 
@@ -94,5 +116,29 @@ public class MainActivity extends AppCompatActivity {
 
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         mTextViewCountDown.setText(timeLeftFormatted);
+
+        if (seconds == 0) {
+            playSound(TAG_SOUND4);
+            resetTimer();
+        }
+    }
+
+    private void playSound(String tag) {
+        try {
+            if (tag.equals(TAG_SOUND1)) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.decision12);
+            } else if (tag.equals(TAG_SOUND2)) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.decision16);
+            } else if (tag.equals(TAG_SOUND3)) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.decision21);
+            } else {
+                mediaPlayer = MediaPlayer.create(this, R.raw.decision24);
+            }
+
+            mediaPlayer.setLooping(false);
+            mediaPlayer.seekTo(0);
+            mediaPlayer.start();
+        } catch (Exception e) {
+        }
     }
 }
